4eeb4e246f684711b964a3ed8039d3bb0548fde2
Siddharth <siddbh6305@gmail.com>
2024-04-25 22:52:14 +0530

    feat: prediction api commit -s

--- routes/prediction.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { predictionTrade } = require("../controllers/prediction");
-
-router.route("/prediction").post(predictionTrade);
-
-module.exports = router;
