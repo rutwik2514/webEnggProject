319b91621b7bf1ffb434db76036c65c09b28ed40
Siddharth <siddbh6305@gmail.com>
2024-04-25 21:34:49 +0530

    Feat: Inital commit -S

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { Register, Login } = require("../controllers/auth");
-
-router.route("/register").post(Register);
-router.route("/login").post(Login);
-
-module.exports = router;
