92d08f88f84cfa1f7153e9f956951d62e7db8c2e
Siddharth <siddbh6305@gmail.com>
2024-04-25 22:58:47 +0530

    Feat: Added modified trade and userdata routes -S

--- routes/trade.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { Buy, Sell } = require("../controllers/trade");
-
-router.route("/buy").patch(Buy);
-router.route("/sell").patch(Sell);
-
-module.exports = router;
--- routes/user.data.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getUserData } = require("../controllers/user.data");
-
-router.route("/dashboard").post(getUserData);
-
-module.exports = router;
